a4343ba8d4aacb160d7ce8cb1f208a17776720b6
Sam Mercado <samueljmercado@yahoo.com>
2019-07-23 22:53:31 -0400

    Create basic components

---
+++ client/src/components/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { browserHistory } from 'react-router';
+import HomePage from './HomePage';
+import NavBar from './HeaderComponent/NavBar';
+
+/* 
+ * This component will also store the routes for the various (React) routes that will be 
+ * used to navigate the website. This is where we use React-Router for the website.  
+ */
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavBar />
+          <Route name="home" exact path="/" component={HomePage} />
+          <Footer />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App;
---
+++ client/src/index.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/App';
+
+// <App /> is the first component to be rendered to the virtual DOM.
+ReactDOM.render(<App />, document.getElementById('app'));
