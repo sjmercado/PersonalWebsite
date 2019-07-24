6872aa0c6c85be6f70c98211d4af94a4ead4f50e
Sam Mercado <samueljmercado@yahoo.com>
2019-07-23 22:34:17 -0400

    Create & configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+/* 
+ * This configuration file defines what the entry and output points are for this application. 
+ * Webpack also needs to be configured to load certain file types that will be used in building 
+ * this website, e.g. a CSS file, image files such as .png and .jpg files, and babel-loader with 
+ * presets. The babel-loader is important as React makes heavy use of ES6, which will be used 
+ * to build components for the application. Babel compiles code written in ES6 to ES5 so that 
+ * all browsers can interpret the JavaScript code correctly.
+ */
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/react-client/src');
+const DIST_DIR = path.join(__dirname, '/react-client/dist');
+const webpack = require('webpack');
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.css']
+  },
+  module : {
+    rules : [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.png$/,
+        loader: 'url-loader?limit=100000&minetype=image/png'
+      },
+      {
+        test: /\.jpg/,
+        loader: 'file-loader'
+      },
+      {
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',      
+        query: {
+          presets: ['react', 'es2015']
+       }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+};
